6b30cb5ffc6ee96c0ae4fcd2eff1f834c69216f9
Mhmmdfhkri <mufik2002@gmail.com>
2023-06-15 21:15:27 +0800

    add model

---
+++ Market-Airland/lib/models/product.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class product{
+  @required String title;
+  @required String imageUrl;
+  @required int price;
+  @required String description;
+
+  product({this.title, this.imageUrl, this.price, this.description})
+} 
